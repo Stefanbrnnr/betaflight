1175d8747024dbfce145a6354d05ba09fef9122f
Stefanbrnnr <stefanbn03@gmail.com>
2025-11-14 22:51:07 +0100

    Update target.mk

--- src/main/target/BRENNER_H7/target.mk
+++ src/main/target/BRENNER_H7/target.mk
@@ -1,25 +1,25 @@
-# MCU family
+# MCU type
 MCU_GROUP := H743X
 TARGET_MCU := STM32H743
+
+# Register target
+H743X_TARGETS += BRENNER_H7
 
 # Board name
 TARGET_BOARD := BRENNER_H7
 
-# Flash + RAM size for H743
+# Flash + RAM
 FLASH_SIZE := 2048
 RAM_SIZE := 512
 
-# Correct linker script for Betaflight H743
+# Linker script for H743
 LINKER_SCRIPT := stm32h743xg
 
-# Ensure Betaflight includes this target
-H743X_TARGETS += BRENNER_H7
-
-# Add only your drivers
+# Drivers
 DRIVERS += gyro_icm42688p
 DRIVERS += baro_bmp388
 
-# Prevent warnings from breaking the build
+# Avoid build errors from warnings
 CFLAGS += -Wno-error
 
 # Required by Betaflight build system
