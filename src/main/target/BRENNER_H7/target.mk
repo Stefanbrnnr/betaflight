abab0d515453712789a5e1d0204e289fe0c857dc
Stefanbrnnr <stefanbn03@gmail.com>
2025-11-14 22:57:44 +0100

    Update MCU group and target definitions in target.mk

--- src/main/target/BRENNER_H7/target.mk
+++ src/main/target/BRENNER_H7/target.mk
@@ -1,26 +1,26 @@
-# MCU type
-MCU_GROUP := H743X
+# MCU family for H743VI
+MCU_GROUP := H743
 TARGET_MCU := STM32H743
 
-# Register target
-H743X_TARGETS += BRENNER_H7
+# Register in correct family
+H743_TARGETS += BRENNER_H7
 
 # Board name
 TARGET_BOARD := BRENNER_H7
 
-# Flash + RAM
+# Flash & RAM size
 FLASH_SIZE := 2048
 RAM_SIZE := 512
 
-# Linker script for H743
+# Linker script (Thunder uses same)
 LINKER_SCRIPT := stm32h743xg
 
 # Drivers
 DRIVERS += gyro_icm42688p
 DRIVERS += baro_bmp388
 
-# Avoid build errors from warnings
+# Avoid Werror breaks
 CFLAGS += -Wno-error
 
-# Required by Betaflight build system
+# Required by BF build system
 TARGET_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
